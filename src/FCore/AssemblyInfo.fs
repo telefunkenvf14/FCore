9f97293be063b88a74ae7ee3ed0eb42abf94f45f
Adam Mlocek <mlocek@yahoo.com>
2015-07-18 19:43:03 +0200

    Initial project scaffold

---
+++ src/FCore/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FCore")>]
+[<assembly: AssemblyProductAttribute("FCore")>]
+[<assembly: AssemblyDescriptionAttribute("F# numerical library")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
